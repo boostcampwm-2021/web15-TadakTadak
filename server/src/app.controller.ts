99f30f8e70f3d6454978c19857ab2d64513cd06d
youngbeom0908 <youngbeom0908@gmail.com>
2021-11-01 18:59:42 +0900

    Chore: #6 - 테스트용 기본 코드 작성
    테스트용 기본 코드를 작성했습니다.
    차후 개발 시 정리할 예정입니다.

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/test')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
   }
