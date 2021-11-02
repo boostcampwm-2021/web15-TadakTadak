5c199d2a306f13d41aad53cde4cdcbfaad03d685
jiho-bae <jiho@dgu.ac.kr>
2021-11-02 13:53:52 +0900

    Chore: #6 - 프론트엔드 eslint, prettier 설정

---
+++ client/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './client/tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'linebreak-style': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
