b9114aaf7fd21d361d1d5aa0080e737fc439ca42
jiho-bae <jiho@dgu.ac.kr>
2021-11-02 17:54:59 +0900

    Feat: #45 - 로그인 화면 form, input, handler 추가
    로그인하는 유저는 email, password를 입력하고 제출하면 인증을 진행할 예정입니다.

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login = (): JSX.Element => {
-  return <div>Login Page</div>;
+const Login: React.FC = (): JSX.Element => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setEmail(e.target.value);
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+    setPassword(e.target.value);
+
+  const onSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!email || !password) {
+      return;
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitForm}>
+        <label htmlFor="email">Email</label>
+        <input type="text" placeholder="Email" id="email" value={email} onChange={onChangeEmail} maxLength={50} />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          value={password}
+          maxLength={15}
+          onChange={onChangePassword}
+        />
+        <input type="submit" value="제출" />
+      </form>
+    </div>
+  );
 };
 export default Login;
