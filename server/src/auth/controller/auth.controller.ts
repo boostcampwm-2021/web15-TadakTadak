809528fb7ecc4d591bb09ed4bb259da301aa000c
zaehuun <jdrjk0000@naver.com>
2021-11-02 22:59:14 +0900

    Chore: #47 - Get 메소드 삭제

--- server/src/auth/controller/auth.controller.ts
+++ server/src/auth/controller/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { LoginRequestDto } from '../dto/loginRequestDTO';
 
