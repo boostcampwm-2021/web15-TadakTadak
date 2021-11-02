f06cae5076b2731f8ebbda04da11d547749659ce
jiho-bae <jiho@dgu.ac.kr>
2021-11-02 16:48:29 +0900

    Feat: #45 - 프론트엔드 로그인화면 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/login" component={Login} />
         <Redirect from="*" to="/" />
       </Switch>
     </BrowserRouter>
---
+++ client/src/pages/Login/Login.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Login = (): JSX.Element => {
+  return <div>Login Page</div>;
+};
+export default Login;
