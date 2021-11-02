9ecaae11c56a3146583241f36df612964cecfbb4
jiho-bae <jiho@dgu.ac.kr>
2021-11-02 22:04:29 +0900

    Design: 프로젝트에 GlobalStyle 적용
    프론트엔드 프로젝트에reset, 사용자정의 전역스타일을 적용했습니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import GlobalStyles from './styles/GlobalStyles';
 
 const App = (): JSX.Element => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Redirect from="*" to="/" />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </BrowserRouter>
+      <GlobalStyles />
+    </>
   );
 };
 
