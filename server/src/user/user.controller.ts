0add4e2f8e3c048b4beb187b5ee63b8a7d0d3d07
Dev-Beom <youngbeom0908@gmail.com>
2021-11-02 13:54:16 +0900

    Chore: #6 - 서버 측 클라인트 ESLint 수정

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -3,10 +3,10 @@ import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) { }
-    
-    @Get()
-    getUserInfo(): string{
-        return this.userService.getUserInfo();
-    }
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getUserInfo(): string {
+    return this.userService.getUserInfo();
+  }
 }
