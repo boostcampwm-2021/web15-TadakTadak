989ecb4e85bde0415fc5e704b380bd64e7db24b3
zaehuun <jdrjk0000@naver.com>
2021-11-02 21:48:55 +0900

    Feat: #47 - TypeOrm 연동 및 유저 엔티티 테이블 생성
    자동으로 엔티티로 등록된 것들을 테이블로 저장할 수 있도록 연동했습니다.
    TypeOrm에 대한 자세한 설정들은 추후 더 알아보며 적용해야 할 거 같습니다.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { RoomModule } from './room/room.module';
 
 @Module({
-  imports: [UserModule, AuthModule, RoomModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UserModule, AuthModule, RoomModule],
 })
 export class AppModule {}
--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './auth.repository';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [AuthController],
   providers: [AuthService],
 })
