4153ed712c23c747d2c66729c0bf2c1109f5c775
zaehuun <jdrjk0000@naver.com>
2021-11-03 22:25:08 +0900

    Refactor: #48 - bcrypt를 객체로 따로 뺌
    bcrypt가 비밀번호 외에 사용될 일은 없어보이지만 service 내에서 존재하는
    것보다 따로 util로 빼서 쓰는게 더 깔끔한 거 같습니당..

--- server/src/auth/service/auth.service.ts
+++ server/src/auth/service/auth.service.ts
@@ -1,7 +1,7 @@
-import * as bcrypt from 'bcrypt';
 import { Injectable, UnauthorizedException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
+import { Bcrypt } from 'src/utils/bcrypt';
 import { User } from '../auth.entity';
 import { AuthRepository } from '../auth.repository';
 import { LoginRequestDto } from '../dto/loginRequestDTO';
@@ -16,7 +16,7 @@ export class AuthService {
 
   async validateUser(loginRequestDTO: LoginRequestDto): Promise<any> {
     const user: User = await this.authRepository.findOne({ where: { email: loginRequestDTO.email } });
-    if (user && (await bcrypt.compare(loginRequestDTO.password, user.password))) return user;
+    if (user && Bcrypt.compare(loginRequestDTO.password, user.password)) return user;
     throw new UnauthorizedException({
       statusCode: HttpStatus.UNAUTHORIZED,
       message: ['USER_INFORMATION_DOES_NOT_MATCH'],
