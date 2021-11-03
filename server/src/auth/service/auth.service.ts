7271466b2dd27a85e936eefee8656dff0984ec54
zaehuun <jdrjk0000@naver.com>
2021-11-03 22:09:30 +0900

    Feat: #48 - Local login에 대한 필터링 설정
    local 로그인을 할 때 이메일, 패스워드에 대해 필터링(?)단에서 처리할 수
    있도록 했습니다.

--- server/src/auth/service/auth.service.ts
+++ server/src/auth/service/auth.service.ts
@@ -1,19 +1,33 @@
 import * as bcrypt from 'bcrypt';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 import { User } from '../auth.entity';
 import { AuthRepository } from '../auth.repository';
 import { LoginRequestDto } from '../dto/loginRequestDTO';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectRepository(AuthRepository) private readonly authRepository: AuthRepository) {}
+  constructor(
+    @InjectRepository(AuthRepository)
+    private readonly authRepository: AuthRepository,
+    private jwtService: JwtService,
+  ) {}
 
-  async login(loginRequestDTO: LoginRequestDto): Promise<boolean> {
-    const user: User = await this.authRepository.findOne({
-      where: { email: loginRequestDTO.email },
+  async validateUser(loginRequestDTO: LoginRequestDto): Promise<any> {
+    const user: User = await this.authRepository.findOne({ where: { email: loginRequestDTO.email } });
+    if (user && (await bcrypt.compare(loginRequestDTO.password, user.password))) return user;
+    throw new UnauthorizedException({
+      statusCode: HttpStatus.UNAUTHORIZED,
+      message: ['USER_INFORMATION_DOES_NOT_MATCH'],
+      error: 'Unauthorized',
     });
-    if (user && (await bcrypt.compare(loginRequestDTO.password, user.password))) return true;
-    return false;
+  }
+
+  async login(loginRequestDTO: LoginRequestDto) {
+    const payload = { email: loginRequestDTO.email };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
   }
 }
