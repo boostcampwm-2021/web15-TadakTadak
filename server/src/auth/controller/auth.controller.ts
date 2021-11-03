4112b4cb3b32bc0af121fc2fc41148145adbd3ed
zaehuun <jdrjk0000@naver.com>
2021-11-03 22:10:40 +0900

    Feat: #48 - 로그인 검증 및 Access token 발급
    로그인 검증을 통해 Access token을 발급할 수 있도록 했습니다.

--- server/src/auth/controller/auth.controller.ts
+++ server/src/auth/controller/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
+import { LocalAuthGuard } from '../guard/local-auth.guard';
 import { LoginRequestDto } from '../dto/loginRequestDTO';
 
 @Controller('/api/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(LocalAuthGuard)
+  // @authPublic()
   @Post('/login')
   login(@Body() loginRequestDto: LoginRequestDto) {
     return this.authService.login(loginRequestDto);
