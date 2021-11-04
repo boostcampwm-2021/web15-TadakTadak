5c2c6f1bc946a1914f9cf829e7b92edd0c39a51e
jiho-bae <jiho@dgu.ac.kr>
2021-11-04 17:52:03 +0900

    Feat: #37 - 메인 사이드바 추가, 로그인 버튼 추가
    - 메인 페이지에 사이드바 추가
    - 로그인 버튼 추가
    - 로그인 버튼 클릭시 모달창을 띄우도록 함

---
+++ client/src/components/SideBar.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import Login from '../pages/Login';
+
+const SideBarContainer = styled.div`
+  padding: ${({ theme }) => theme.paddings.lg};
+  display: flex;
+  flex-direction: column;
+  width: 23vw;
+  height: 100%;
+  background-color: #21272e;
+`;
+
+const LoginBtn = styled.button`
+  ${({ theme }) => css`
+    border: 1px solid ${theme.colors.primary};
+    padding: ${theme.paddings.sm};
+    border-radius: ${theme.borderRadius.sm};
+    font-size: ${theme.fontSizes.lg};
+  `};
+  width: 100%;
+  text-align: center;
+`;
+
+const SideBar: React.FC = () => {
+  const [loginModal, setLoginModal] = useState(false);
+
+  const onClick = () => setLoginModal(!loginModal);
+  return (
+    <SideBarContainer>
+      <LoginBtn onClick={onClick}>로그인</LoginBtn>
+      {loginModal && <Login />}
+    </SideBarContainer>
+  );
+};
+
+export default SideBar;
--- client/src/pages/Main/style.ts
+++ client/src/pages/Main/style.ts
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 
+export const MainWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`;
+
 export const MainContainer = styled.div`
   padding: ${({ theme }) => theme.paddings.lg};
+  height: 100%;
 `;
 
 export const MainTitle = styled.h1`
