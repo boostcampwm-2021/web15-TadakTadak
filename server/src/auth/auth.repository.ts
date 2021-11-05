b9ca42b602cf3ae5220c2686c8909ec337740659
youngbeom0908 <youngbeom0908@gmail.com>
2021-11-05 23:34:12 +0900

    Refactor: Snake case 대응

--- server/src/auth/auth.repository.ts
+++ server/src/auth/auth.repository.ts
@@ -1,12 +1,12 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { User } from './auth.entity';
+import { User } from '../user/user.entity';
 
 @EntityRepository(User)
 export class AuthRepository extends Repository<User> {
   async exists(user): Promise<boolean> {
     return (
       (await this.createQueryBuilder('user')
-        .where('user.nickname = :nickname OR user.email = :email', {
+        .where('user.nick_name = :nick_name OR user.email = :email', {
           nickname: user.nickname,
           email: user.email,
         })
