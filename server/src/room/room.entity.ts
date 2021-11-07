b4d36f539b90032a1f5a5446777a4cc90c565664
youngbeom0908 <youngbeom0908@gmail.com>
2021-11-08 03:12:39 +0900

    Refactor: Room Entity 의존성 주입

--- server/src/room/room.entity.ts
+++ server/src/room/room.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { User } from 'src/user/user.entity';
 
-enum RoomType {
+export enum RoomType {
   TadakTadak = '타닥타닥',
   CampFire = '캠프파이어',
   CodingLive = '코딩라이브',
