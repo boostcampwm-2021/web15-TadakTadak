55ed6a276c46705e22dfb7adccf7cb059d57aaaf
youngbeom0908 <youngbeom0908@gmail.com>
2021-11-08 16:58:48 +0900

    Refactor: Ground rule 반영

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
+import { TypeOrmConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { RoomModule } from './room/room.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule, AuthModule, RoomModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UserModule, AuthModule, RoomModule],
 })
 export class AppModule {}
