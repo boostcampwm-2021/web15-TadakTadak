a7a4443db530aec6056d6ae5bc24cdf55597d246
zaehuun <jdrjk0000@naver.com>
2021-11-09 17:46:20 +0900

    Refactor: #116 - 로그인 인증처리를 가드에서 서비스로 전환한다.
    코드의 복잡도를 줄이기 위해 로그인 가드를 서비스에서 처리할 수 있도록
    수정했습니다.

--- server/src/auth/guard/local-auth.guard.ts
+++
@@ -1,6 +0,0 @@
-//auth/guards/local-auth.guard.ts
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
