cf562fcbc5ad3c83b69adef7c078500b72dbcc98
zaehuun <jdrjk0000@naver.com>
2021-11-09 16:44:56 +0900

    Feat: #160 - 인터셉터, 예외 처리 필터 등록
    서버에서 사용할 수 있도록 인터셉터와 예외 처리 필터를 등록했습니다.

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,10 +4,14 @@ import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 dotenv.config();
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './utils/http-exception.filter';
+import { TransformInterceptor } from './utils/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
