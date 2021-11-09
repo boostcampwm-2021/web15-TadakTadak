5db40f7fe4e8795c83f1e9245c10c42893310763
jiho-bae <jiho@dgu.ac.kr>
2021-11-09 17:17:23 +0900

    Feat: #50 - 접속/새로고침 시 토큰으로 유저정보 요청
    - App.tsx에서 액세스토큰으로 유저정보 요청
    - 유효한 요청이면 유저컨텍스트에 유저정보 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,28 @@
+import { useCallback, useEffect } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import Introduction from '@pages/Introduction';
 import Main from '@pages/Main';
+import { useUser, useUserFns } from '@contexts/userContext';
+import { getUserByToken } from '@utils/apis';
 
 const App = (): JSX.Element => {
+  const user = useUser();
+  const { logUserIn } = useUserFns();
+
+  const getUser = useCallback(async () => {
+    const { data } = await getUserByToken();
+    if (data) {
+      logUserIn(data);
+    }
+  }, [logUserIn]);
+
+  useEffect(() => {
+    if (!user?.nickname) {
+      getUser();
+    }
+  }, [getUser, user]);
+
   return (
     <>
       <BrowserRouter>
