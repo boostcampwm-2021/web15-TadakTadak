2029e29a54ceabe87f192fc3a6aa7e2a3809dd6c
youngbeom0908 <youngbeom0908@gmail.com>
2021-11-09 18:39:41 +0900

    Refactor: DA 로직이 많아질 것 같아 Repository로 분리

---
+++ server/src/domain/room/repository/room.repository.ts
@@ -0,0 +1,18 @@
+import { EntityRepository, Like, Repository } from 'typeorm';
+import { Room, RoomType } from '../room.entity';
+import { CreateRoomRequestDto } from '../dto/create-room-request.dto';
+import { PaginationOptions } from '../../../paginate';
+
+@EntityRepository(Room)
+export class RoomRepository extends Repository<Room> {
+  async findByKeywordAndCount(options: PaginationOptions, roomType: RoomType): Promise<[Room[], number]> {
+    const { search, take, page } = options;
+    return this.findAndCount({
+      where: { title: Like('%' + search + '%'), roomType: roomType },
+      order: { title: 'DESC' },
+      take: take,
+      skip: take * (page - 1),
+    });
+  }
+
+}
