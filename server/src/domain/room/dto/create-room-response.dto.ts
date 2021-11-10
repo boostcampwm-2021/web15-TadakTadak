71e5f0526a810cf113d657cce559579bae257cd4
Dev-Beom <youngbeom0908@gmail.com>
2021-11-11 00:33:55 +0900

    Refactor: 방 생성 응답 DTO 임시 제거

--- server/src/domain/room/dto/create-room-response.dto.ts
+++ server/src/domain/room/dto/create-room-response.dto.ts
@@ -1,25 +1,10 @@
 import { Room } from '../room.entity';
 
 export class CreateRoomResponseDto {
-  appId: string;
-  token: string;
-  uuid: string;
-  ownerId: number;
-  title: string;
-  description: string;
-  nowHeadcount: number;
-  maxHeadcount: number;
-  roomType: string;
+  private room: Room;
 
-  constructor(room: Room, appID: string, token: string, uuid: string) {
-    this.appId = appID;
-    this.token = token;
-    this.uuid = uuid;
-    this.ownerId = room.owner.id;
-    this.title = room.title;
-    this.description = room.description;
-    this.nowHeadcount = room.nowHeadcount;
-    this.maxHeadcount = room.maxHeadcount;
-    this.roomType = room.roomType;
+  constructor(room: Room) {
+    //Todo: Owner(User) password hide
+    this.room = room;
   }
 }
