22aef0846a6d79a6b0add16c84bf806109c4bf51
zaehuun <jdrjk0000@naver.com>
2021-11-10 18:29:45 +0900

    Feat: #129 - S3 이미지 도메인 분리
    재사용 할 수도 있기에 서비스만 따로 도메인으로 분리했습니다.

---
+++ server/src/domain/image/service/image.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
+
+@Injectable()
+export class ImageService {
+  private s3: AWS.S3;
+
+  constructor() {
+    const endpoint = new AWS.Endpoint(process.env.NCP_OBJ_STORAGE_END_POINT);
+    const region = process.env.NCP_REGION;
+    this.s3 = new AWS.S3({
+      endpoint,
+      region,
+      credentials: {
+        accessKeyId: process.env.NCP_ACCESS_KEY,
+        secretAccessKey: process.env.NCP_SECRET_KEY,
+      },
+    });
+  }
+
+  async uploadImage(image) {
+    const param = {
+      Bucket: process.env.NCP_BUCKET_NAME,
+      Key: `images/${Date.now()}-${image.originalname}`,
+      ACL: 'public-read',
+      Body: image.buffer,
+    };
+    return new Promise((resolve, reject) => {
+      this.s3.upload(param, (err, data) => {
+        if (err) {
+          reject(err.message);
+        }
+        resolve(data);
+      });
+    });
+  }
+}
