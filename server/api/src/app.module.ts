357fceab2bce1e5fb8319d7dd4f344dd296b9466
Dev-Beom <youngbeom0908@gmail.com>
2021-11-16 00:11:57 +0900

    Feat: API 서버 Rate Limiter 설정

--- server/api/src/app.module.ts
+++ server/api/src/app.module.ts
@@ -6,15 +6,25 @@ import { AuthModule } from './domain/auth/auth.module';
 import { RoomModule } from './domain/room/room.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { SchedulerModule } from './scheduler/scheduler.module';
+import { RateLimiterGuard, RateLimiterModule } from 'nestjs-rate-limiter';
+import { APP_GUARD } from '@nestjs/core';
+import { RateLimiterConfig } from './config/rateLimiter.config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(TypeOrmConfig),
+    RateLimiterModule.register(RateLimiterConfig),
     ScheduleModule.forRoot(),
     SchedulerModule,
     UserModule,
     AuthModule,
     RoomModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RateLimiterGuard,
+    },
   ],
 })
 export class AppModule {}
