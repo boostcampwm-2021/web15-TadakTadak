cb9e4af6f76361b71b8b727cce81f3364b1390cc
Narastro <advice02@gmail.com>
2021-11-16 18:57:09 +0900

    Chore: 로그인 입력 input 태그 자동완성 On

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -113,15 +113,7 @@ const LoginForm: React.FC<LoginProps> = ({ onClickModalToggle, setModal }) => {
   return (
     <Container>
       <Form onSubmit={onSubmitForm}>
-        <Input
-          type="text"
-          placeholder="Email"
-          id="email"
-          value={email}
-          onChange={onChangeEmail}
-          maxLength={50}
-          autoComplete="new-password"
-        />
+        <Input type="text" placeholder="Email" id="email" value={email} onChange={onChangeEmail} maxLength={50} />
         <Input
           type="password"
           placeholder="Password"
