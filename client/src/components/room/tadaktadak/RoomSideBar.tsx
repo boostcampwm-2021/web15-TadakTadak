5b7294217aaeaa6a77544eeff4acaf30345109b7
jiho-bae <jiho@dgu.ac.kr>
2021-11-16 18:04:28 +0900

    Feat: 방 사이드바에 참가자 목록 연동
    - 방 사이드바에서 참가자 탭 선택시 참가자 목록을 표시
    - 참가자를 더미데이터로 구성

--- client/src/components/room/tadaktadak/RoomSideBar.tsx
+++ client/src/components/room/tadaktadak/RoomSideBar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Tab from '@components/Tab';
+import ParticipantList from './ParticipantList';
 
 const SIDEBAR_MIN_WIDTH = '29rem';
 const SIDEBAR_HEIGHT = '100vh';
@@ -9,7 +10,6 @@ const SideBarContainer = styled.div`
   padding: ${({ theme }) => theme.paddings.lg};
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   width: ${SIDEBAR_MIN_WIDTH};
   min-width: ${SIDEBAR_MIN_WIDTH};
   height: ${SIDEBAR_HEIGHT};
@@ -18,9 +18,11 @@ const SideBarContainer = styled.div`
 
 const SideBarTopMenus = styled.div``;
 
-const SideBarBottomMenus = styled.div``;
+const SideBarBottomMenus = styled.div`
+  height: 100%;
+`;
 
-const SideBarTaps = styled.div`
+const SideBarTabs = styled.div`
   display: flex;
 `;
 
@@ -31,19 +33,26 @@ const initialTabState = {
 
 const RoomSideBar: React.FC = () => {
   const [tabs, setTabs] = useState({ ...initialTabState });
+  const [participants, setParticipants] = useState([
+    { nickname: 'Tom' },
+    { nickname: 'James' },
+    { nickname: 'Work' },
+    { nickname: 'Bob' },
+  ]);
+  const { isChat, isParticipant } = tabs;
 
-  const onClickChatTap = () => setTabs({ ...initialTabState, isChat: !tabs.isChat });
-  const onClickParticipantTap = () => setTabs({ ...initialTabState, isParticipant: !tabs.isParticipant });
+  const onClickChatTap = () => setTabs({ ...initialTabState, isChat: !isChat });
+  const onClickParticipantTap = () => setTabs({ ...initialTabState, isParticipant: !isParticipant });
 
   return (
     <SideBarContainer>
       <SideBarTopMenus>
-        <SideBarTaps>
-          <Tab text="채팅" isActive={tabs.isChat} onClick={onClickChatTap} />
-          <Tab text="참가자" isActive={tabs.isParticipant} onClick={onClickParticipantTap} />
-        </SideBarTaps>
+        <SideBarTabs>
+          <Tab text="채팅" isActive={isChat} onClick={onClickChatTap} />
+          <Tab text="참가자" isActive={isParticipant} onClick={onClickParticipantTap} />
+        </SideBarTabs>
       </SideBarTopMenus>
-      <SideBarBottomMenus></SideBarBottomMenus>
+      <SideBarBottomMenus>{isParticipant && <ParticipantList participants={participants} />}</SideBarBottomMenus>
     </SideBarContainer>
   );
 };
