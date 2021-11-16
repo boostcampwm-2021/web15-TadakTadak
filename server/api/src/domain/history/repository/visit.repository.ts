f75b35ccde7b1cbd07e5cd9bc623212228fb4be8
zaehuun <jdrjk0000@naver.com>
2021-11-16 22:23:25 +0900

    Feat: 접속자 통계 테이블 추가

---
+++ server/api/src/domain/history/repository/visit.repository.ts
@@ -0,0 +1,18 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { LocalDate } from 'js-joda';
+import { Visit } from '../visit.entity';
+
+@EntityRepository(Visit)
+export class VisitRepository extends Repository<Visit> {
+  async getVisitCount() {
+    const yesterDay: LocalDate = LocalDate.now().minusDays(1);
+    return await this.find({ where: { date: yesterDay } });
+  }
+
+  async addVisitCount(count) {
+    const visit: Visit = new Visit();
+    visit.date = LocalDate.now().minusDays(1);
+    visit.totalVisit = count;
+    this.save(visit);
+  }
+}
