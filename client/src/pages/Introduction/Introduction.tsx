6e1cdcc58483c39251d0fe869a14a859da9c50b2
Narastro <advice02@gmail.com>
2021-11-16 14:37:14 +0900

    Feat: #9 - 소개 페이지 글귀 창작

--- client/src/pages/Introduction/Introduction.tsx
+++ client/src/pages/Introduction/Introduction.tsx
@@ -1,14 +1,44 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { useUser } from '@contexts/userContext';
 import { useEffect, useState } from 'react';
 import FireAnimation from '@components/FireAnimation';
 
-const IntroContainer = styled.div`
+interface IntroContainerProps {
+  fireOn: boolean;
+}
+
+const IntroContainer = styled.div<IntroContainerProps>`
   ${({ theme }) => theme.flexCenter};
   flex-direction: column;
   width: 100vw;
   height: 100vh;
+  background-color: ${({ theme }) => theme.colors.bgGreen};
+  ${(props) =>
+    props.fireOn &&
+    css`
+      background-color: inherit;
+    `}
+  transition: background-color 1800ms linear;
   padding: ${({ theme }) => theme.paddings.base};
+  font-family: 'Dongle', sans-serif;
+`;
+
+const IntroTitle = styled.div`
+  z-index: 2;
+  color: white;
+  font-size: 200px;
+`;
+const IntroDescription = styled.div`
+  z-index: 2;
+  color: white;
+  font-size: 40px;
+  margin-bottom: ${({ theme }) => theme.margins.xl};
+`;
+const IntroButtonDescription = styled.div`
+  z-index: 2;
+  color: white;
+  font-size: ${({ theme }) => theme.fontSizes.xl};
+  margin-top: 70px;
 `;
 
 const Introduction = (props: { history: { push(url: string): void } }): JSX.Element => {
@@ -29,8 +59,11 @@ const Introduction = (props: { history: { push(url: string): void } }): JSX.Elem
   }, [fireOn, push]);
 
   return (
-    <IntroContainer>
+    <IntroContainer fireOn={fireOn}>
+      <IntroTitle>타닥타닥</IntroTitle>
+      <IntroDescription>개발자들을 위한 아늑한 공간</IntroDescription>
       <FireAnimation setFireOn={setFireOn} />
+      <IntroButtonDescription>모닥불을 피워 주세요</IntroButtonDescription>
     </IntroContainer>
   );
 };
