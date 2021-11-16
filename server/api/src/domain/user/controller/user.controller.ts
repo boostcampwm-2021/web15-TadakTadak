e5d31ad88bdc8fd473fc355b7326625696a511e6
zaehuun <jdrjk0000@naver.com>
2021-11-16 20:08:05 +0900

    Refactor: 타입 추가

--- server/api/src/domain/user/controller/user.controller.ts
+++ server/api/src/domain/user/controller/user.controller.ts
@@ -41,14 +41,14 @@ export class UserController {
   @Post('/:userId/image')
   @UseGuards(JwtAuthGuard)
   @UseInterceptors(FileInterceptor('image'))
-  async uploadUserImage(@Param('userId') nickname: string, @UploadedFile() file) {
+  async uploadUserImage(@Param('userId') nickname: string, @UploadedFile() file: Express.Multer.File) {
     return { result: await this.userService.updateImage(nickname, file) };
   }
 
   @Patch('/:userId/image')
   @UseGuards(JwtAuthGuard)
   @UseInterceptors(FileInterceptor('image'))
-  async patchUserImage(@Param('userId') nickname: string, @UploadedFile() file) {
+  async patchUserImage(@Param('userId') nickname: string, @UploadedFile() file: Express.Multer.File) {
     return { result: await this.userService.updateImage(nickname, file) };
   }
 
