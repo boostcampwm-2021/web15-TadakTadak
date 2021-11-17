d9c545ca2102ad586d9d268c434491dd9597a890
zaehuun <jdrjk0000@naver.com>
2021-11-17 19:32:27 +0900

    Refactor: 스케줄러, user 리포로 변경

--- server/api/src/scheduler/scheduler.service.ts
+++ server/api/src/scheduler/scheduler.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
-import { AuthRepository } from 'src/domain/auth/auth.repository';
+import { UserRepository } from 'src/domain/user/repository/user.repository';
 import { VisitRepository } from 'src/domain/history/repository/visit.repository';
 
 @Injectable()
 export class SchedulerService {
   constructor(
-    @InjectRepository(AuthRepository)
-    private readonly authRepository: AuthRepository,
+    @InjectRepository(UserRepository)
+    private readonly userRepository: UserRepository,
     @InjectRepository(VisitRepository)
     private readonly visitRepository: VisitRepository,
   ) {}
   @Cron('0 0 * * *')
   async handleCron() {
-    const count = await this.authRepository.getLastVisitCount();
+    const count = await this.userRepository.getLastVisitCount();
     this.visitRepository.addVisitCount(count);
   }
 }
