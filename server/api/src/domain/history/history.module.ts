216bf7b4431920123c23956788fbe281713012eb
zaehuun <jdrjk0000@naver.com>
2021-11-17 16:36:30 +0900

    Refactor: find -> findOne으로 수정

--- server/api/src/domain/history/history.module.ts
+++ server/api/src/domain/history/history.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HistoryController } from './controller/history.controller';
 import { HistoryService } from './service/history.service';
-import { HistoryRepository } from './repository/history.repository';
 import { AuthRepository } from '../auth/auth.repository';
+import { HistoryRepository } from './repository/history.repository';
 import { VisitRepository } from './repository/visit.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([HistoryRepository, AuthRepository, VisitRepository])],
+  controllers: [HistoryController],
   providers: [HistoryService],
   exports: [HistoryService],
 })
