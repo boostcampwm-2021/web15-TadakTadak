7572f1a45519f8b8eeddfad0f5b1d718bf00ba13
youngbeom0908 <youngbeom0908@gmail.com>
2021-11-24 02:12:23 +0900

    Refactor: 스크립트 설명 추가 및 Readme 추가

--- script/install_docker.sh
+++ script/install_docker.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
 echo "🔥 docker를 설치합니다."
 
-echo "🔥 apt package를 업데이트 합니다."
 sudo apt update
 sudo apt-get install -y ca-certificates \ 
     curl \
@@ -8,19 +8,11 @@ sudo apt-get install -y ca-certificates \
     apt-transport-https \
     gnupg \
     lsb-release
-echo "🔥 apt package 업데이트 되었습니다."
 
-echo "🔥 GPG 키 및 저장소를 추가합니다."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-echo "🔥 GPG 키 및 저장소를 추가했습니다."
-
-echo "🔥 도커 엔진을 설치합니다."
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io
-echo "🔥 도커 엔진이 설치 완료되었습니다."
-
-echo "🔥 설치된 도커의 정보입니다."
+echo "🔥 docker 설치가 완료되었습니다."
 docker info
-echo "🔥 docker를 설치가 완료되었습니다."
--- script/setting_all.sh
+++ script/setting_all.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-# 리눅스 커널이 최소 3.10 버전 이상이여야 합니다.
-# sudo 권한 혹은 root 권한을 소유한 계정에서 설치 진행해야 합니다.
-# 최신 배포판 OS인지 확인해야 합니다.
-# 64bit 운영체제이여야 합니다.
-
 sudo sh ./install_docker.sh
 sudo sh ./install_nodejs.sh
