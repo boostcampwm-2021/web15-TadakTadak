df72f699e913bd14c2850f70de4c8c966cde6368
youngbeom0908 <youngbeom0908@gmail.com>
2021-11-23 21:23:33 +0900

    Feat: Remote Redis Server 연결
    - env 추가하고, Excel 갱신했어요

--- server/socket/src/redis.adapter.ts
+++ server/socket/src/redis.adapter.ts
@@ -1,8 +1,14 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { createAdapter } from 'socket.io-redis';
 import { RedisClient } from 'redis';
+import { Logger } from '@nestjs/common';
 
-export const pubClient = new RedisClient({ host: 'localhost', port: 6379 });
+const HOST: string = process.env.DB_REDIS_HOST ?? 'localhost';
+const PORT: number = process.env.DB_REDIS_PORT ? parseInt(process.env.DB_REDIS_PORT) : 6379;
+export const pubClient = new RedisClient({
+  host: HOST,
+  port: PORT,
+});
 export const subClient = pubClient.duplicate();
 
 const redisAdapter = createAdapter({ pubClient, subClient });
@@ -11,6 +17,7 @@ export class RedisIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: any): any {
     const server: any = super.createIOServer(port, options);
     server.adapter(redisAdapter);
+    Logger.log(`Redis Adapted ${process.env.DB_REDIS_HOST}:${PORT}`);
     return server;
   }
 }
