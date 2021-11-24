6d1372c99fe4df06b993bfe50d22b6a1bd9ca879
jiho-bae <jiho@dgu.ac.kr>
2021-11-25 03:44:45 +0900

    Feat: 공통으로 사용할 사이드바 컴포넌트 추가

---
+++ client/src/components/common/SideBar.tsx
@@ -0,0 +1,39 @@
+import styled, { css } from 'styled-components';
+import { SIDEBAR } from '@utils/styleConstant';
+
+const SideBarContainer = styled.div`
+  ${({ theme }) => css`
+    ${theme.flexColumn};
+    padding: ${theme.paddings.lg};
+    background-color: ${theme.colors.white};
+    border: 1px solid ${theme.colors.borderGrey};
+  `};
+  justify-content: space-between;
+  width: ${SIDEBAR.minWidth};
+  min-width: ${SIDEBAR.minWidth};
+  height: ${SIDEBAR.height};
+  z-index: 10;
+`;
+
+const SideBarTopMenus = styled.div``;
+
+const SideBarBottomMenus = styled.div<{ bottomMenuHeight?: string }>`
+  height: ${({ bottomMenuHeight }) => bottomMenuHeight};
+`;
+
+interface SideBarProps {
+  topMenus: React.ReactNode;
+  bottomMenus: React.ReactNode;
+  bottomMenuHeight?: string;
+}
+
+const SideBar = ({ topMenus, bottomMenus, bottomMenuHeight }: SideBarProps): JSX.Element => {
+  return (
+    <SideBarContainer>
+      <SideBarTopMenus>{topMenus}</SideBarTopMenus>
+      <SideBarBottomMenus bottomMenuHeight={bottomMenuHeight}>{bottomMenus}</SideBarBottomMenus>
+    </SideBarContainer>
+  );
+};
+
+export default SideBar;
