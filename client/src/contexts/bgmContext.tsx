20c3504008c9e90c4474f5ac9047bd9713699511
jiho-bae <jiho@dgu.ac.kr>
2021-11-25 12:41:56 +0900

    Feat: #85 - 배경음악 파일과 BGMContext 추가
    - BGMContext로 컴포넌트를 감싸서 배경음을 사용할 수 있다.
    - 기본적으로 autoplay를 제공하나, 처음에 사용자가 먼저 재생을 눌러야 한다.

---
+++ client/src/contexts/bgmContext.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback, useContext, useState } from 'react';
+
+const campfireSoundPath = process.env.PUBLIC_URL + 'campfire-sound.mp3';
+
+interface PlayFnProps {
+  togglePlay: () => void;
+}
+
+interface BGMContextProps {
+  isPlay: boolean;
+  fn: PlayFnProps;
+}
+
+export const BGMContext = React.createContext<BGMContextProps>({ isPlay: false, fn: { togglePlay: () => {} } });
+
+const BGMContextProvider = ({ children }: { children: React.ReactNode }): JSX.Element => {
+  const [isPlay, setIsPlay] = useState(false);
+  const togglePlay = useCallback(() => {
+    setIsPlay((prevState) => !prevState);
+  }, [setIsPlay]);
+
+  return (
+    <BGMContext.Provider value={{ isPlay, fn: { togglePlay } }}>
+      {children}
+      {isPlay && <audio src={campfireSoundPath} autoPlay={true}></audio>}
+    </BGMContext.Provider>
+  );
+};
+
+export const usePlayBgm = (): boolean => {
+  const { isPlay } = useContext(BGMContext);
+  return isPlay;
+};
+
+export const usePlayBgmFns = (): PlayFnProps => {
+  const { fn } = useContext(BGMContext);
+  return fn;
+};
+
+export default BGMContextProvider;
