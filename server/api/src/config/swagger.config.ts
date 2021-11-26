896b8a206094f7055813ea275c1c483576d22c53
youngbeom0908 <youngbeom0908@gmail.com>
2021-11-27 03:30:06 +0900

    Fix: Socket Server Cors

--- server/api/src/config/swagger.config.ts
+++ server/api/src/config/swagger.config.ts
@@ -6,7 +6,7 @@ export function SwaggerConfig(app: INestApplication): void {
   const option = new DocumentBuilder()
     .setTitle('타닥타닥 API 문서')
     .setDescription('타닥타닥의 API 문서입니다.')
-    .setContact('타닥타닥 개발팀', 'https://tadaktadak.com', null)
+    .setContact('타닥타닥 개발팀', 'https://tadaktadak.com', '')
     .setVersion('1.0.0')
     .build();
   const document = SwaggerModule.createDocument(app, option);
