715e0369abd5e89e22010fd61b6c8e53cfc1accf
zaehuun <jdrjk0000@naver.com>
2021-11-28 00:12:57 +0900

    Feat: 타입 지정

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -49,68 +49,79 @@ export const chatTimeFormatting = (time?: string): string => {
   return '';
 };
 
-export const getWidths = (line: any) => {
+export const getWidths = (width: number): Array<number> => {
   return Array.from({ length: 12 }, (_, i) => {
     if (i === 0) return 0;
-    return (line.width / 12) * i;
+    return (width / 12) * i;
   });
 };
-export const getHeights = (months: any, maxMonths: any, line: any) => {
+export const getHeights = (months: Array<number>, maxMonths: number, height: number): Array<number> => {
   return Array.from({ length: 12 }, (_, i) => {
-    if (months[i + 1]) return (months[i + 1] / maxMonths) * line.height * 0.8;
+    if (months[i + 1]) return (months[i + 1] / maxMonths) * height * 0.8;
     return 0;
   });
 };
 
-export const drawLineChartYaxis = (ctx: any, line: any) => {
+export const drawLineChartYaxis = (ctx: CanvasRenderingContext2D, width: number, height: number): void => {
   ctx.fillStyle = '#888888';
   for (let i = 2; i <= 24; i++) {
-    ctx.moveTo(line.width - i * (line.width / 24) + 40, 0);
-    ctx.lineTo(line.width - i * (line.width / 24) + 40, line.height - 30);
+    ctx.moveTo(width - i * (width / 24) + 40, 0);
+    ctx.lineTo(width - i * (width / 24) + 40, height - 30);
     ctx.stroke();
-    if (i % 2 === 0) ctx.fillText(13 - i / 2, line.width - i * (line.width / 24) + 40, line.height);
+    if (i % 2 === 0) ctx.fillText((13 - i / 2).toString(), width - i * (width / 24) + 40, height);
   }
 };
 
-export const drawLineChartXaxis = (ctx: any, line: any) => {
+export const drawLineChartXaxis = (ctx: CanvasRenderingContext2D, width: number, height: number): void => {
   ctx.strokeStyle = '#f5f5f5';
   ctx.font = '12px Noto Sans KR';
   ctx.textAlign = 'center';
   for (let i = 1; i <= 15; i++) {
-    ctx.moveTo(40, line.height - i * (line.height / 10) + 10);
-    ctx.lineTo(line.width - 40, line.height - i * (line.height / 10) + 8);
+    ctx.moveTo(40, height - i * (height / 10) + 10);
+    ctx.lineTo(width - 40, height - i * (height / 10) + 8);
     ctx.stroke();
   }
 };
 
-export const drawLineChartLines = (widths: any, ctx: any, heights: any, line: any) => {
+export const drawLineChartLines = (
+  widths: Array<number>,
+  ctx: CanvasRenderingContext2D,
+  heights: Array<number>,
+  height: number,
+): void => {
   let currentX = 40,
-    currentY = line.height - 30;
+    currentY = height - 30;
 
-  widths.forEach(async (w: any, i: any) => {
+  widths.forEach(async (w: number, i: number) => {
     ctx.beginPath();
     ctx.lineWidth = 2;
     await new Promise((res) => setTimeout(res, 50 * i));
     ctx.moveTo(currentX, currentY);
     ctx.strokeStyle = '#ee7f6e';
-    ctx.lineTo(w + 40, line.height - heights[i] - 30);
+    ctx.lineTo(w + 40, height - heights[i] - 30);
     ctx.stroke();
     currentX = w + 40;
-    currentY = line.height - heights[i] - 30;
+    currentY = height - heights[i] - 30;
   });
 };
 
-export const drawLineChartDots = (ctx: any, widths: any, heights: any, months: any, line: any) => {
+export const drawLineChartDots = (
+  ctx: CanvasRenderingContext2D,
+  widths: Array<number>,
+  heights: Array<number>,
+  months: any,
+  height: number,
+): void => {
   ctx.beginPath();
-  widths.forEach((w: any, i: any) => {
+  widths.forEach((w: number, i: number) => {
     ctx.fillStyle = '#ee7f6e';
     if (heights[i] > 0) {
       ctx.beginPath();
-      ctx.arc(w + 40, line.height - heights[i] - 30, 6, 0, Math.PI * 2);
+      ctx.arc(w + 40, height - heights[i] - 30, 6, 0, Math.PI * 2);
       ctx.closePath();
       ctx.fill();
       ctx.fillStyle = '#ee7f6e';
-      ctx.fillText(months[i + 1], w + 40, line.height - heights[i] - 40);
+      ctx.fillText(months[i + 1], w + 40, height - heights[i] - 40);
     }
   });
   ctx.stroke();
