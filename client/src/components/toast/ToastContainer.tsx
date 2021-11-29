3a5d8152eeb5e0b099b706fc40f74351d0b88d92
Narastro <advice02@naver.com>
2021-11-29 17:19:43 +0900

    Refactor: 불필요한 클래스 제거

--- client/src/components/toast/ToastContainer.tsx
+++ client/src/components/toast/ToastContainer.tsx
@@ -5,8 +5,8 @@ export default function ToastContainer(): JSX.Element {
   const { toasts } = useToastStateContext();
 
   return (
-    <div className="absolute bottom-10 w-full z-50">
-      <div className="max-w-xl mx-auto">
+    <div>
+      <div>
         {toasts &&
           toasts.map((toast) => <Toast id={toast.id} key={toast.id} type={toast.type} message={toast.message} />)}
       </div>
