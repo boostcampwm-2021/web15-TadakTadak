8a789d07bd080908098463534847018eb0a3a5cf
Narastro <advice02@gmail.com>
2021-11-30 12:09:47 +0900

    Refactor: VideoList 스타일 별도 분리

---
+++ client/src/components/video/VideoList/style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const VideosContainer = styled.div`
+  ${({ theme }) => theme.flexCenter}
+  height: 100%;
+`;
+
+export const VideoBoxWrapper = styled.div`
+  ${({ theme }) => theme.flexColumn}
+`;
+
+export const UserInfoDiv = styled.div`
+  width: 100%;
+  ${({ theme }) => theme.flexCenter}
+  margin-top: ${({ theme }) => theme.margins.sm};
+  font-size: ${({ theme }) => theme.fontSizes.xl};
+`;
+
+export const VideosGrid = styled.div`
+  padding: ${({ theme }) => theme.paddings.lg};
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  gap: 2rem;
+`;
