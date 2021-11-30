842b17f9f620a196486372c0d19c46c4a04fac14
zaehuun <jdrjk0000@naver.com>
2021-12-01 06:57:21 +0900

    Refactor: 이번에도 안 되면 취침

--- server/api/src/domain/auth/strategy/jwt.strategy.ts
+++ server/api/src/domain/auth/strategy/jwt.strategy.ts
@@ -2,8 +2,8 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Request } from 'express';
-import 'dotenv/config';
-// import * as Process from 'process';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
