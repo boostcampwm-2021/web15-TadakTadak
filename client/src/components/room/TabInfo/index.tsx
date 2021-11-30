4a352386d62753536d441f54fc63ac6723a45d78
Narastro <advice02@gmail.com>
2021-11-30 12:34:44 +0900

    Refactor: TabInfo 스타일 분리 및 수정

--- client/src/components/room/TabInfo/index.tsx
+++ client/src/components/room/TabInfo/index.tsx
@@ -1,55 +1,19 @@
-import styled from 'styled-components';
-import React from 'react';
 import { AiFillQuestionCircle } from 'react-icons/all';
+import { TooltipTarget, TooltipBox, StyledTabInfo } from './style';
 
 interface ToolTipProps {
   text?: string;
 }
 
-const TooltipTarget = styled.span`
-  cursor: pointer;
-`;
-
-const TooltipBox = styled.div`
-  position: absolute;
-  top: calc(100% - 5px);
-  left: calc(100% - 55px);
-  visibility: hidden;
-  color: transparent;
-  background-color: transparent;
-  width: 200px;
-  padding: 5px 5px;
-  border-radius: 4px;
-  z-index: 1;
-  transition: visibility 0.3s, color 0.3s, background-color 0.3s, width 0.3s, padding 0.3s ease-in-out;
-`;
-
-const StyledQuestionButton = styled.span`
-  margin-left: 1rem;
-  vertical-align: middle;
-  position: relative;
-
-  & ${TooltipTarget}:hover + ${TooltipBox} {
-    visibility: visible;
-    color: #fff;
-    background-color: rgba(0, 0, 0, 0.5);
-    padding: 8px 8px;
-
-    &:before {
-      border-color: transparent transparent rgba(0, 0, 0, 0.8) rgba(0, 0, 0, 0.8);
-    }
-  }
-`;
-
-const QuestionButton = ({ text }: ToolTipProps): JSX.Element => {
+const TabInfo = ({ text }: ToolTipProps): JSX.Element => {
   return (
-    <StyledQuestionButton>
+    <StyledTabInfo>
       <TooltipTarget>
         <AiFillQuestionCircle />
       </TooltipTarget>
       <TooltipBox>{text}</TooltipBox>
-    </StyledQuestionButton>
+    </StyledTabInfo>
   );
 };
 
-export default QuestionButton;
+export default TabInfo;
---
+++ client/src/components/room/TabInfo/style.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const TooltipTarget = styled.span`
+  cursor: pointer;
+`;
+
+export const TooltipBox = styled.div`
+  position: absolute;
+  top: calc(100% - 5px);
+  left: calc(100% - 55px);
+  visibility: hidden;
+  color: transparent;
+  background-color: transparent;
+  width: 200px;
+  padding: 5px 5px;
+  border-radius: 4px;
+  z-index: 1;
+  transition: visibility 0.3s, color 0.3s, background-color 0.3s, width 0.3s, padding 0.3s ease-in-out;
+`;
+
+export const StyledQuestionButton = styled.span`
+  margin-left: 1rem;
+  vertical-align: middle;
+  position: relative;
+
+  & ${TooltipTarget}:hover + ${TooltipBox} {
+    visibility: visible;
+    color: #fff;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 8px 8px;
+
+    &:before {
+      border-color: transparent transparent rgba(0, 0, 0, 0.8) rgba(0, 0, 0, 0.8);
+    }
+  }
+`;
