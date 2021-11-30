7f2492dffaa38ab3028a2bef41d6536f0138f525
youngbeom0908 <youngbeom0908@gmail.com>
2021-11-30 18:34:28 +0900

    Refactor: RoomService에서 사용되는 의존성 추가

--- server/api/src/domain/room/service/room.service.spec.ts
+++ server/api/src/domain/room/service/room.service.spec.ts
@@ -3,6 +3,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RoomService } from './room.service';
 import { RoomRepository } from '../repository/room.repository';
 import { UserRepository } from '../../user/repository/user.repository';
+import { RoomBuilder, UserBuilder } from '../../../builder';
+import { datatype, internet, lorem } from 'faker';
+import { RoomType } from '../room.entity';
+import { DevFieldBuilder } from '../../../builder/dev-field.builder';
+import { RoomResponseDto } from '../dto/room-response.dto';
+import { RoomException } from '../../../exception';
+import { BadRequestException, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { UpdateResult } from 'typeorm';
 
 describe('RoomService', () => {
   let roomService: RoomService;
