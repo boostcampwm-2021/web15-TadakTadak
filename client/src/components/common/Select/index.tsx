357d3089e4b1bc9a7b7f048c61840af855432cd0
Narastro <advice02@gmail.com>
2021-11-30 14:30:21 +0900

    Refactor: Select컴포넌트 -> 디렉토리 형식으로 변경

--- client/src/components/common/Select/index.tsx
+++ client/src/components/common/Select/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import { StyledSelect } from './style';
 
 interface OptionProps {
   value: string | number;
@@ -12,30 +12,6 @@ interface SelectProps {
   options: OptionProps[];
   onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }
-
-const StyledSelect = styled.select`
-  width: 100%;
-  height: 4rem;
-  font-family: 'Dongle', sans-serif;
-  ${({ theme }) => css`
-    color: ${theme.colors.black};
-    font-size: ${theme.fontSizes.lg};
-    font-weight: ${theme.fontWeights.bold};
-    padding: ${theme.paddings.sm};
-    border: 1px solid ${theme.colors.borderGrey};
-    border-radius: ${theme.borderRadius.base};
-    background-color: ${theme.colors.white};
-    ${theme.flexCenter}
-  `}
-  &:hover {
-  }
-  &:active {
-  }
-  &:focus {
-  }
-  &:disabled {
-  }
-`;
 
 const Select = ({ name, className, options, onChange }: SelectProps): JSX.Element => {
   return (
