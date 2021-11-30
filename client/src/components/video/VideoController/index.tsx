d2b2a3ec3c7318c777a282a36d10327c55a3fa5b
Narastro <advice02@gmail.com>
2021-11-30 12:24:03 +0900

    Refactor: config.ts 이름 변경으로 인한 사이드이펙트

--- client/src/components/video/VideoController/index.tsx
+++ client/src/components/video/VideoController/index.tsx
@@ -8,7 +8,7 @@ import { SocketEvents } from '@socket/socketEvents';
 import { ButtonContainer, Controls, Getout } from './style';
 import Button from '@components/common/Button';
 import ScreenShare from '../Screenshare';
-import { useClient } from '../videoConfig';
+import { useClient } from '../config';
 import { deleteRoom } from '@src/apis';
 import { useUser } from '@contexts/userContext';
 import { useTheme } from '@contexts/themeContext';
