fb7743a48909d850d73a51a09d3c6441262c6805
Narastro <advice02@gmail.com>
2021-11-30 15:01:52 +0900

    Refactor: ParticipantList 컴포넌트 sideBar로 이동

--- client/src/components/sideBar/ParticipantList/index.tsx
+++ client/src/components/sideBar/ParticipantList/index.tsx
@@ -1,8 +1,8 @@
 import { useCallback } from 'react';
-import styled, { css } from 'styled-components';
-import { FieldName, useUser } from '@src/contexts/userContext';
 import socket from '@socket/socket';
 import { SocketEvents } from '@socket/socketEvents';
+import { Container, List, Participant, Avatar, Nickname, DevField, Position, GetOutBtn } from './style';
+import { FieldName, useUser } from '@src/contexts/userContext';
 import { useTheme } from '@contexts/themeContext';
 
 interface ParticipantListProps<T> {
@@ -10,63 +10,6 @@ interface ParticipantListProps<T> {
   hostNickname: string | undefined;
   uuid: string;
 }
-
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  ${({ theme }) => theme.flexColumn};
-`;
-
-const List = styled.ul`
-  width: 100%;
-  ${({ theme }) => theme.flexColumn};
-  padding: ${({ theme }) => theme.paddings.sm};
-`;
-
-const Participant = styled.li`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  margin-bottom: ${({ theme }) => theme.margins.sm};
-`;
-
-const Avatar = styled.img`
-  margin-right: ${({ theme }) => theme.margins.base};
-  width: 3rem;
-  height: 3rem;
-  border-radius: 50%;
-  overflow: hidden;
-`;
-
-const Nickname = styled.span`
-  font-size: ${({ theme }) => theme.fontSizes.lg};
-`;
-
-const DevField = styled.div<{ bgColor: string }>`
-  background-color: ${({ bgColor }) => bgColor};
-  ${({ theme }) => css`
-    margin-left: ${theme.margins.base};
-    padding: ${theme.paddings.sm};
-    border-radius: ${theme.borderRadius.sm};
-  `}
-`;
-
-const Position = styled.div`
-  margin-left: ${({ theme }) => theme.margins.xs};
-`;
-
-const GetOutBtn = styled.div`
-  ${({ theme }) => css`
-    margin-left: ${theme.margins.base};
-    padding: ${theme.paddings.xs};
-    border-radius: ${theme.borderRadius.sm};
-    :hover {
-      background-color: ${theme.colors.primary};
-    }
-  `}
-  cursor: pointer;
-`;
 
 const ParticipantList = ({
   participants,
