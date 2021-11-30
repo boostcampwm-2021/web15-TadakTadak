2635cecc7e854177ecbb3dab2fdf0a718db056fb
zaehuun <jdrjk0000@naver.com>
2021-11-30 18:45:29 +0900

    Refactor: user-response-dto 중복 제거

--- server/api/src/domain/auth/dto/user-response.dto.ts
+++
@@ -1,9 +0,0 @@
-import { DevField } from '../../field/dev-field.entity';
-
-export class UserResponseDto {
-  readonly id: number;
-  readonly nickname: string;
-  readonly email: string;
-  readonly imageUrl: string;
-  readonly devField: DevField;
-}
