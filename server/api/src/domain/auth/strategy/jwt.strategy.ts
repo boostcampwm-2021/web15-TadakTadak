0ab3b28fa4b4e9121b8f320eaef4fb3f1334ac54
zaehuun <jdrjk0000@naver.com>
2021-12-01 17:02:58 +0900

    Refactor: test

--- server/api/src/domain/auth/strategy/jwt.strategy.ts
+++ server/api/src/domain/auth/strategy/jwt.strategy.ts
@@ -2,15 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Request } from 'express';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import * as Process from 'process';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: Process.env.JWT_SECRET,
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: Request) => {
           const data = request?.cookies['access-token'];
