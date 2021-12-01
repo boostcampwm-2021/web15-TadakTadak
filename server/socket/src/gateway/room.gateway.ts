13088d3e8490360cf185fc325f564f16d15aef5b
Dev-Beom <youngbeom0908@gmail.com>
2021-12-02 01:44:02 +0900

    Fix: 시간 오류 수정

--- server/socket/src/gateway/room.gateway.ts
+++ server/socket/src/gateway/room.gateway.ts
@@ -14,7 +14,7 @@ import { LocalDateTime, ZoneId } from '@js-joda/core';
 import { RoomEvent } from './room.event';
 import { RoomService } from './room.service';
 
-@WebSocketGateway({ cors: true })
+@WebSocketGateway({ cors: true, allowEIO3: true })
 export class RoomGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('AppGateway');
@@ -25,10 +25,11 @@ export class RoomGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
   handleMessage(client: Socket, { uuid, message, nickname }: IMessage): void {
     const emitMessage: IMessage = {
       message: `${message} from ${process.env.NODE_PORT}`,
-      time: LocalDateTime.now(ZoneId.of('Asia/Seoul')),
+      time: LocalDateTime.now().plusHours(9),
       nickname: nickname,
       uuid: uuid,
     };
+    this.logger.log(emitMessage);
     this.server.to(uuid).emit(RoomEvent.MsgToClient, emitMessage);
   }
 
