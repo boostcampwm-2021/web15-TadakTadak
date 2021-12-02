51fc3bfda21b97dadb863ea8034634b8862766e1
Narastro <advice02@gmail.com>
2021-12-02 11:52:18 +0900

    Feat: 메인 타이틀에 이스터 에그 추가
    유저의 흥미를 유발할 수 있는 요소를 넣었습니다.

--- client/src/components/ServiceInfo/index.tsx
+++ client/src/components/ServiceInfo/index.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
-import { MainTitleLink, MainInfo } from './style';
+import { MainTitle, MainInfo } from './style';
 import { getVisitCount } from '@src/apis';
-import { PATH } from '@src/utils/constant';
+import { useToast } from '@hooks/useToast';
 
 function ServiceInfo(): JSX.Element {
   const [visit, setVisit] = useState<number>();
+  const toast = useToast();
 
   const getServiceInfo = async () => {
     const { isOk, data } = await getVisitCount();
@@ -13,13 +14,17 @@ function ServiceInfo(): JSX.Element {
     }
   };
 
+  const onClickTitle = () => {
+    toast('easterEgg', '안녕하세요 TadakTadak팀입니다.　　숨겨진 이스터에그를 찾아보세요...!');
+  };
+
   useEffect(() => {
     getServiceInfo();
   }, []);
 
   return (
     <>
-      <MainTitleLink to={PATH.main}>타닥타닥</MainTitleLink>
+      <MainTitle onClick={onClickTitle}>타닥타닥</MainTitle>
       <MainInfo>🔥어제 모닥불을 피운 사용자는 {visit}명입니다🔥</MainInfo>
     </>
   );
