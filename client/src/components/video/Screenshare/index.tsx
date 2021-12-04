df801c866f439d4abf9a6ef17b78b31c965ff6ca
jiho-bae <jiho@dgu.ac.kr>
2021-12-04 18:17:59 +0900

    Fix: #303 - 첫 화면공유시 unmount로 인한 트랙닫힘버그 수정
    - 화면공유시 간헐적으로 unmount에 달린 트랙닫힘로직이 수행되던 문제
    - firstRenderRef 변수를 도입하여 트랙변화로 인한 첫 unmount시에,
    - 트랙닫힘로직을 수행하지 않도록 개선
    - 두번째 unmount부터 화면닫힘로직이 수행되므로,
    - 화면공유 후 페이지 이탈시 기존의 트랙닫힘로직 실행 가능

--- client/src/components/video/Screenshare/index.tsx
+++ client/src/components/video/Screenshare/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { ICameraVideoTrack, IMicrophoneAudioTrack } from 'agora-rtc-react';
 import { useClient, useScreenVideoTrack } from '@components/video/config';
 
@@ -19,6 +19,7 @@ const ScreenShare = ({
 }: ScreenShareDivProps): JSX.Element => {
   const client = useClient();
   const { ready, tracks, error } = useScreenVideoTrack();
+  const firstRenderRef = useRef(true);
 
   useEffect(() => {
     const pulishScreenShare = async () => {
@@ -39,6 +40,10 @@ const ScreenShare = ({
     if (error) setScreenShare(false);
 
     return () => {
+      if (firstRenderRef.current) {
+        firstRenderRef.current = false;
+        return;
+      }
       if (!error && !Array.isArray(tracks)) {
         client.unpublish(tracks);
         tracks.close();
